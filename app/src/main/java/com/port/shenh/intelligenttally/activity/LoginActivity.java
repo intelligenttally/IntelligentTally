670fd8c0211683eff6bd3835c1cba7c03292fadb
shenh <shenh2013@163.com>
2016-11-08 11:10:10 +0800

    修改登录页面布局，修改用户名和密码ID，关联库

---
+++ app/src/main/java/com/port/shenh/intelligenttally/activity/LoginActivity.java
@@ -0,0 +1,51 @@
+package com.port.shenh.intelligenttally.activity;
+/**
+ * Created by sh on 2016/11/7.
+ */
+
+
+import android.content.Intent;
+
+import com.port.shenh.intelligenttally.R;
+
+import org.mobile.library.model.activity.BaseLoginActivity;
+
+/**
+ * 用户登录Activity
+ *
+ * @author sh
+ * @version 2.0 2016/11/7
+ * @since 1.0
+ */
+
+public class LoginActivity extends BaseLoginActivity {
+
+
+    @Override
+    protected int onActivityLoginLayout() {
+        return R.layout.activity_login;
+    }
+
+    @Override
+    protected boolean onSetCenterTitle() {
+        return true;
+    }
+
+    @Override
+    protected boolean onLoginSuccess(String message) {
+
+        goMain();
+        return false;
+    }
+
+    /**
+     * 跳转到主界面
+     */
+    private void goMain() {
+        Intent mainIntent = new Intent(this, MainActivity.class);
+        startActivity(mainIntent);
+        finish();
+    }
+
+
+}
---
+++ app/src/main/java/com/port/shenh/intelligenttally/util/StaticValue.java
@@ -0,0 +1,49 @@
+package com.port.shenh.intelligenttally.util;
+/**
+ * Created by sh on 2015/11/7.
+ */
+
+/**
+ * 全局常量
+ *
+ * @author sh
+ * @version 1.0 2016/11/7
+ * @since 1.0
+ */
+public interface StaticValue {
+
+    /**
+     * 应用代码
+     */
+    String APP_CODE = "ZNLH";
+
+    /**
+     * 应用令牌
+     */
+    String APP_TOKEN = "3D24DC97C489B3EAE053A864016AB3EA";
+
+    /**
+     * 意图数据传递标签
+     */
+    interface IntentTag {
+
+    }
+
+    /**
+     * 网络请求接口地址
+     */
+    interface Url {
+        /**
+         * IP
+         */
+        String HTTP_IP_URL = "http://218.92.115.55/";
+
+        /**
+         * 登录
+         */
+        String HTTP_LOGIN_URL = "M_ZNLH/Entrance/Login.aspx";
+
+
+    }
+
+}
