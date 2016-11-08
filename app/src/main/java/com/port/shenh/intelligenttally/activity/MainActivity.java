60a32f86f3e4aa0db0ab23cd8d3c349fccd729bd
shenh <shenh2013@163.com>
2016-11-08 10:40:29 +0800

    启动页

--- app/src/main/java/com/port/shenh/intelligenttally/activity/MainActivity.java
+++ app/src/main/java/com/port/shenh/intelligenttally/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.port.shenh.intelligenttally;
+package com.port.shenh.intelligenttally.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.port.shenh.intelligenttally.R;
 
 public class MainActivity extends AppCompatActivity {
 
